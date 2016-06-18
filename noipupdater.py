ddcb2288cddf90a178091b23d1e0a1ae6059160c
Lucas Zanela <me@lucaszanella.com>
2016-06-17 22:10:19 -0300

    Update noipupdater.py

--- noipupdater.py
+++ noipupdater.py
@@ -5,10 +5,12 @@ from http.client import HTTPSConnection
 from base64 import b64encode
 
 #User customizable part *********
-interval = 15 #in seconds
-noIpDomain = 'something.no-ip.org'
-user = 'username'
-password = 'password'
+interval = 5 #in seconds
+noIpDomain = 'mydomain.no-ip.org'
+user = 'myusername@email.com
+password = 'myPassword'
+n = 3 # number of update tries
+printSuccess = False #makes it print the success message in every try
 #end ****************************
 
 
@@ -18,12 +20,16 @@ userAndPassEncoded = b64encode(UserAndPasswordInBytes).decode("ascii")
 headers = { 'Authorization' : 'Basic %s' %  userAndPassEncoded }
 
 while True:
-    try:
-        c = HTTPSConnection("dynupdate.no-ip.com")
-        c.request('GET', '/nic/update?hostname='+noIpDomain, headers=headers)
-        res = c.getresponse()
-        data = res.read()  
-        print(data)
-    except:
-        print('error')
-    time.sleep(interval)
+    for x in range(0,n):
+        try:
+            c = HTTPSConnection("dynupdate.no-ip.com")
+            c.request('GET', '/nic/update?hostname='+noIpDomain, headers=headers)
+            res = c.getresponse()
+            data = res.read()
+            if "nochg" in data.decode("utf-8"):
+                if printSuccess:
+                    print(data)
+                break;  
+        except:
+            print('error')
+        time.sleep(interval)
